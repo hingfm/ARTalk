f7f01027f939f1e94d52bf2832573651e9434ac4
hingfm <45617277+hingfm@users.noreply.github.com>
2024-01-27 23:47:52 +0800

    Update APIKeys.swift
    input own api key

--- ARTalk/Utility/APIKeys.swift
+++ ARTalk/Utility/APIKeys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 public enum APIKeys {
-    public static let openAI = "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"
-    public static let sketchfab = "24aaf964154a4e89a672d7483fc9c9c7"
+    public static let openAI = ""
+    public static let sketchfab = ""
 }
