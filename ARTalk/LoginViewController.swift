0410743dc4064acc10ca779af4114259276eb67a
hohing <hing.filemaker@gmail.com>
2024-01-28 16:47:38 +0800

    add login view controller

---
+++ ARTalk/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  ARTalk
+//
+//  Created by Hing Chung on 28/1/2024.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseCore
+
+class LoginViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
